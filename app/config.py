fb65afdc5ff6d690f27cabf843512ed36ecd5bf3
Andrew Hamilton <andrew.hamilton@gmail.com>
2026-04-27 12:40:57 +0000

    update sqlite env var and filename in config file

--- app/config.py
+++ app/config.py
@@ -3,10 +3,7 @@ from pathlib import Path
 
 
 class Config:
-    DB_PATH = os.environ.get(
-        "LOC_DB_PATH",
-        str(Path(__file__).resolve().parent.parent / "loc.sqlite3"),
-    )
+    DB_PATH = os.environ.get("TIMELINE_DB_PATH")
     OWNTRACKS_USERNAME = os.environ.get("OWNTRACKS_USERNAME", "phone")
     OWNTRACKS_PASSWORD = os.environ.get("OWNTRACKS_PASSWORD")
     VIEWER_USERNAME = os.environ.get("VIEWER_USERNAME", "viewer")

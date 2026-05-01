64590bb19790f0eaab4d80e8259f3faf79dc1512
Andrew <>
2026-05-01 14:33:46 -0400

    added get locations api

--- app/config.py
+++ app/config.py
@@ -8,3 +8,6 @@ class Config:
     OWNTRACKS_PASSWORD = os.environ.get("OWNTRACKS_PASSWORD")
     VIEWER_USERNAME = os.environ.get("VIEWER_USERNAME", "viewer")
     VIEWER_PASSWORD = os.environ.get("VIEWER_PASSWORD")
+    API_TOKEN = os.environ.get("API_TOKEN")
+    API_MAX_LIMIT = int(os.environ.get("API_MAX_LIMIT", "50000"))
+    API_DEFAULT_LIMIT = int(os.environ.get("API_DEFAULT_LIMIT", "10000"))
